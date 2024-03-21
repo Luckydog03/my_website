d0c54dd537a317e787cd83c31607d3082bcd11d5
Anthony Donsereaux <antwig03@gmail.com>
2024-03-21 13:44:50 -0600

    get resume on experience page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea } from '@mui/material';
+import Resume from "../assets/DonsereauxResume.jpg";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Experience() {
   return (
-    <div>
-      <p>Experience Page </p>
-    </div>
+    <Card>
+      <Grid>
+        <CardActionArea>
+            <a href = {Resume} target = "DonsereauxResume.pdf">
+              <CardMedia component="img" image={Resume} alt="Resume" />
+            </a>
+        </CardActionArea>
+      </Grid>
+    </Card>
   );
 }
