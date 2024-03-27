888b8d6ee78f4166a5141cab17b2def050ffbdce
Anthony Donsereaux <antwig03@gmail.com>
2024-03-27 00:00:18 -0600

    index.js file

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import Projects from "./Projects";
+import About from "./About";
+import Experience from "./Experience";
+
+export {
+    Home,
+    Projects,
+    About,
+    Experience,
+}
