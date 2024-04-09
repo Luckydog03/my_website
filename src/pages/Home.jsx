ca45cac3d0fe4d145e300d8c6145faf1a4982cfe
Anthony Donsereaux <antwig03@gmail.com>
2024-04-09 15:00:38 -0600

    fix homepage button links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Container, Button, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <Container>
       <Grid container direction="column" justifyContent="space-between" alignItems="stretch" >
-        <Button variant="contained" href="/About" sx={{mb:2}}>
-          About Me
-        </Button>
-        <Button variant="contained" href="/Experience" sx={{mb:2}}>
-          My Experience
-        </Button>
-        <Button variant="contained" href="/Projects" sx={{mb:2}}>
-          My Projects
-        </Button>
+        
+          <Button component={Link} to="/About" variant="contained" sx={{mb:2}}>
+            About Me
+          </Button>
+        
+          <Button component={Link} to="/Experience" variant="contained"sx={{mb:2}}>
+            My Experience
+          </Button>
+
+          <Button component={Link} to="/Projects" variant="contained" sx={{mb:2}}>
+            My Projects
+          </Button>
+
       </Grid>
     </Container>
   );
